d82c9d64f9546a7bf22b1cad6752bdf17ec1c2c1
Johnson Chu <johnsoncodehk@gmail.com>
2024-10-09 16:44:50 +0800

    feat: add native-signals

---
+++ src/frameworks/nativeSignals.ts
@@ -0,0 +1,39 @@
+import {
+  computed,
+  effect,
+  EffectScope,
+  effectScope,
+  signal,
+  System,
+} from "native-signals";
+import { ReactiveFramework } from "../util/reactiveFramework";
+
+let prevEffectScope: EffectScope | undefined;
+
+export const nativeFramework: ReactiveFramework = {
+  name: "native-signals",
+  signal: (initial) => {
+    const data = signal(initial);
+    return {
+      read: () => data.get(),
+      write: (v) => data.set(v),
+    };
+  },
+  computed: (fn) => {
+    const c = computed(fn);
+    return {
+      read: () => c.get(),
+    };
+  },
+  effect: (fn) => effect(fn),
+  withBatch: (fn) => {
+    System.startBatch();
+    fn();
+    System.endBatch();
+  },
+  withBuild: (fn) => {
+    prevEffectScope?.stop();
+    prevEffectScope = effectScope();
+    return prevEffectScope.run(fn);
+  },
+};
